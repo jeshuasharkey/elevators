3ceca90ba2e7fdce4173773f8fdb4e7124a06307
Jeshua Sharkey <jeshua@launchagent.co.nz>
2022-03-02 18:44:09 -0500

    make PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const withOffline = require('next-offline');
+
+module.exports = withOffline(nextConfig);
