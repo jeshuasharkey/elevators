a7b21eedc322da4b6fc2cefebe813879a7e2e812
Jeshua Sharkey <jeshua@launchagent.co.nz>
2022-09-30 11:40:06 -0400

    update sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -28,37 +28,37 @@ self.addEventListener('fetch', function (event) {
   );
 });
 
-// self.addEventListener('activate', function (event) {
-//   event.waitUntil(
-//     caches.keys().then(function (cacheNames) {
-//       return Promise.all(
-//         cacheNames
-//           .filter(function (cacheName) {
-//             if (
-//               !cacheName.startsWith(staticCacheName) &&
-//               !cacheName.startsWith(dynamicCacheName)
-//             ) {
-//               return true;
-//             }
-//           })
-//           .map(function (cacheName) {
-//             // completely deregister for ios to get changes too
-//             console.log('deregistering Serviceworker');
-//             if ('serviceWorker' in navigator) {
-//               navigator.serviceWorker
-//                 .getRegistrations()
-//                 .then(function (registrations) {
-//                   registrations.map((r) => {
-//                     r.unregister();
-//                   });
-//                 });
-//               window.location.reload(true);
-//             }
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            if (
+              !cacheName.startsWith(staticCacheName) &&
+              !cacheName.startsWith(dynamicCacheName)
+            ) {
+              return true;
+            }
+          })
+          .map(function (cacheName) {
+            // completely deregister for ios to get changes too
+            console.log('deregistering Serviceworker');
+            if ('serviceWorker' in navigator) {
+              navigator.serviceWorker
+                .getRegistrations()
+                .then(function (registrations) {
+                  registrations.map((r) => {
+                    r.unregister();
+                  });
+                });
+              window.location.reload(true);
+            }
 
-//             console.log('Removing old cache.', cacheName);
-//             return caches.delete(cacheName);
-//           })
-//       );
-//     })
-//   );
-// });
+            console.log('Removing old cache.', cacheName);
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+});
