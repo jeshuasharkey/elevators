fac6a40dc27b04ed97d0be4ddba10d33c3054b4d
Jeshua Sharkey <jeshua@launchagent.co.nz>
2022-10-01 22:07:21 -0400

    remove unused

--- components/Search.tsx
+++ components/Search.tsx
@@ -46,40 +46,6 @@ export default function Search() {
         <FullCard item={activeItem} i={0} overlayStyle={true} />
         <MoreMenu />
       </BottomSheet>
-
-      {/* <Sheet isOpen={!!activeItem} onClose={() => handleClose()}>
-        <Sheet.Container>
-          <Sheet.Header />
-          <Sheet.Content>
-            <FullCard item={activeItem} i={0} overlayStyle={true} />
-          </Sheet.Content>
-        </Sheet.Container>
-        <Sheet.Backdrop />
-      </Sheet> */}
-
-      {/* <AnimatePresence>
-        {activeItem && (
-          <div className='fixed top-0 left-0 right-0 bottom-0 overflow-scroll pt-12 w-screen max-w-screen h-screen flex items-end'>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.2 }}
-              className='bg-[rgb(235,240,244,0.8)] fixed top-0 left-0 w-full h-full'
-              onClick={() => handleClose()}
-            ></motion.div>
-            <motion.div
-              initial={{ y: '110%' }}
-              animate={{ y: 0 }}
-              exit={{ y: '110%' }}
-              transition={{ duration: 0.4 }}
-              className='z-10 relative mt-auto'
-            >
-              <FullCard item={activeItem} i={0} overlayStyle={true} />
-            </motion.div>
-          </div>
-        )}
-      </AnimatePresence> */}
     </>
   );
 }
